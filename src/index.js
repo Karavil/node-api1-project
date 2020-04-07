bd20b95e846eb5ba3549096ae44a0e714df512cd
Alex Karavil <alex@karavil.io>
2020-04-06 21:14:36 -0400

    Added typescript support to project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+// Create a new express app instance
+var app = express();
+console.log("hello");
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import express = require("express");
+// Create a new express app instance
+const app: express.Application = express();
+console.log("hello");
